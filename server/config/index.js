dd892dd9825533045c31179533dd6d420d158d6e
szy0syz <szy0syz@gmail.com>
2018-02-02 00:28:37 +0800

    update 改造server的中间件加载模式

---
+++ server/config/index.js
@@ -0,0 +1,7 @@
+export default {
+  wechat: {
+    appID: 'wxf414b2f007bc1f9f',
+    appsecret: 'ebad50d9a09d3955599217b8cd82b278',
+    token: 'xxxxxxxx'
+  }
+}
--- server/index.js
+++ server/index.js
@@ -1,22 +1,36 @@
 import Koa from 'koa'
 import { Nuxt, Builder } from 'nuxt'
+import R from 'ramda'
+import { resolve } from 'path';
+
+// 25分钟~~~
 
 // Import and Set Nuxt.js options
 let config = require('../nuxt.config.js')
-config.dev = !(app.env === 'production')
+config.dev = !(process.env === 'production')
 
+const r = path => resolve(__dirname, path)
 const host = process.env.HOST || '127.0.0.1'
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4040
+const MIDDLEWARES = ['router']
 
 class Server {
   constructor() {
     this.app = new Koa()
-    //this.userMiddleWare(this.app)
+    this.useMiddleWare(this.app)(MIDDLEWARES)
   }
 
   // 加载不同中间件
+  // 原理：到指定文件夹下扫描所有文件，然后逐个丢require加载，然后再逐个把app对象传到所加载到的对象并执行
   useMiddleWare(app) {
-
+    // app.use(mid1)
+    // app.use(mid2)
+    // app.use(mid3)
+    return R.map()(R.compose(
+      R.map(i => i(app)),
+      require,
+      i => `${r('./middlewares')}/${i}`
+    ))
   }
 
   async start() {
