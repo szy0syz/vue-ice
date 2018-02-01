2bac234b775ee80c1c881196f7ec76dc1dd91839
szy0syz <szy0syz@gmail.com>
2018-02-01 23:58:49 +0800

    update 改造原脚手架中server文件 改成类的方式

--- server/index.js
+++ server/index.js
@@ -1,39 +1,51 @@
 import Koa from 'koa'
 import { Nuxt, Builder } from 'nuxt'
 
-async function start () {
-  const app = new Koa()
-  const host = process.env.HOST || '127.0.0.1'
-  const port = process.env.PORT || 3000
+// Import and Set Nuxt.js options
+let config = require('../nuxt.config.js')
+config.dev = !(app.env === 'production')
 
-  // Import and Set Nuxt.js options
-  let config = require('../nuxt.config.js')
-  config.dev = !(app.env === 'production')
+const host = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || 3000
 
-  // Instantiate nuxt.js
-  const nuxt = new Nuxt(config)
-
-  // Build in development
-  if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
+class Server {
+  constructor() {
+    this.app = new Koa()
+    //this.userMiddleWare(this.app)
   }
 
-  app.use(async (ctx, next) => {
-    await next()
-    ctx.status = 200 // koa defaults to 404 when it sees that status is unset
-    return new Promise((resolve, reject) => {
-      ctx.res.on('close', resolve)
-      ctx.res.on('finish', resolve)
-      nuxt.render(ctx.req, ctx.res, promise => {
-        // nuxt.render passes a rejected promise into callback on error.
-        promise.then(resolve).catch(reject)
+  // 加载不同中间件
+  useMiddleWare(app) {
+
+  }
+
+  async start() {
+    // Instantiate nuxt.js
+    const nuxt = new Nuxt(config)
+
+    // Build in development
+    if (config.dev) {
+      const builder = new Builder(nuxt)
+      await builder.build()
+    }
+
+    this.app.use(async (ctx, next) => {
+      await next()
+      ctx.status = 200 // koa defaults to 404 when it sees that status is unset
+      return new Promise((resolve, reject) => {
+        ctx.res.on('close', resolve)
+        ctx.res.on('finish', resolve)
+        nuxt.render(ctx.req, ctx.res, promise => {
+          // nuxt.render passes a rejected promise into callback on error.
+          promise.then(resolve).catch(reject)
+        })
       })
     })
-  })
 
-  app.listen(port, host)
-  console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
+    this.app.listen(port, host)
+    console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
+  }
 }
 
-start()
+const app = new Server()
+app.start()
