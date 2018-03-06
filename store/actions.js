48e45964faf1ec7c23953125b405411fc8af55b5
Jerry Shi <szy0syz@gmail.com>
2018-03-07 00:09:15 +0800

    update [第一遍] 5-4 家族主页

--- store/actions.js
+++ store/actions.js
@@ -31,5 +31,15 @@ export default {
     state.cities = res.data.data
 
     return res
+  },
+
+  async showHouse({ state }, _id) {
+    if(_id === state.currentHouse._id) return
+
+    const res = await Services.fetchHouse(_id)
+
+    state.currentHouse = res.data.data
+
+    return res
   }
 }
