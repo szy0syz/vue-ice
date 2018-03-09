f6f62344a10c2459b8f218b218a43298bb06d18b
Jerry Shi <szy0syz@gmail.com>
2018-03-09 15:09:18 +0800

    update [第一遍]5-6 nav导航组件

--- components/nav.vue
+++ components/nav.vue
@@ -1,4 +1,51 @@
-<template lang="pug">
-  nav#nav
-    div 首页
+<template lang='pug'>
+  nav#nav(v-if='navVisible')
+    nuxt-link(v-for='(item, index) in navList' :to='item.path' :key='index')
+      div(v-if='index === 0')
+        img(v-if='activeRoute !== item.name' src='~static/img/home.png')
+        img(v-else-if src='~static/img/home-selected.png')
+      div(v-if='index === 1')
+        img(v-if='activeRoute !== item.name' src='~static/img/shopping.png')
+        img(v-else src='~static/img/shopping-selected.png')
+      div(v-if='index === 2')
+        img(v-if='activeRoute !== item.name' src='~static/img/user.png')
+        img(v-else src='~static/img/user-selected.png')
+      p {{item.text}}
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      navList: [
+        {
+          path: '/',
+          name: 'index',
+          text: '冰火脸谱'
+        },
+        {
+          path: '/shopping',
+          name: 'shopping',
+          text: '周边手办'
+        },
+        {
+          path: '/user',
+          name: 'user',
+          text: '我的账户'
+        }
+      ]
+    }
+  },
+
+  computed: {
+    activeRoute() {
+      return this.$route.name
+    },
+    navVisible() {
+      return ['index', 'shopping', 'user'].indexOf(this.activeRoute) > -1
+    }
+  }
+}
+</script>
+
+<style lang='sass' scoped src='static/sass/nav.sass'></style>
