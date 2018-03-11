91ef57f711a113aaa4b571f61396f83f951d0029
szy0syz <szy0syz@gmail.com>
2018-03-11 22:01:23 +0800

    update 6-9 暂时关闭家族数据批量插入

--- server/middlewares/database.js
+++ server/middlewares/database.js
@@ -10,16 +10,16 @@ fs.readdirSync(models)
   .filter(file => ~file.search(/\.js$/)) // 只要.js结尾的文件
   .forEach(file => require(resolve(models, file)))
 
-const formateData = R.map(i => {
-  i._id = i.umId
+// const formateData = R.map(i => {
+//   i._id = i.umId
 
-  return i
-})
+//   return i
+// })
 
-let wikiCharacters = require(resolve(__dirname, '../../completeCharacters.json'))
-let wikiHouses = require(resolve(__dirname, '../../completeHouses.json'))
+// let wikiCharacters = require(resolve(__dirname, '../../completeCharacters.json'))
+// let wikiHouses = require(resolve(__dirname, '../../completeHouses.json'))
 
-wikiCharacters = formateData(wikiCharacters)
+// wikiCharacters = formateData(wikiCharacters)
 
 export const database = app => {
   mongoose.set('debug', true) // 开发环境打印详细日志
@@ -37,13 +37,13 @@ export const database = app => {
   mongoose.connection.on('open', async () => {
     console.log('Connected to MongoDB ', config.db)
 
-    const WikiHouse = mongoose.model('WikiHouse')
-    const WikiCharacter = mongoose.model('WikiCharacter')
+    // const WikiHouse = mongoose.model('WikiHouse')
+    // const WikiCharacter = mongoose.model('WikiCharacter')
 
-    const existWikiHouses = await WikiHouse.find({}).exec()
-    const existWikiCharacters = await WikiCharacter.find({}).exec()
+    // const existWikiHouses = await WikiHouse.find({}).exec()
+    // const existWikiCharacters = await WikiCharacter.find({}).exec()
 
-    if (!existWikiHouses.length) WikiHouse.insertMany(wikiHouses)
-    if (!existWikiCharacters.length) WikiHouse.insertMany(wikiCharacters)
+    // if (!existWikiHouses.length) WikiHouse.insertMany(wikiHouses)
+    // if (!existWikiCharacters.length) WikiHouse.insertMany(wikiCharacters)
   })
 }
