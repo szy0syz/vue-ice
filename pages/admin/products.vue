5b1bce56d07eb137c6ea5df82492bea11c5cd22b
szy0syz <szy0syz@gmail.com>
2018-03-18 23:51:39 +0800

    update [7-6] 配置七牛上传图片底层方法

--- pages/admin/products.vue
+++ pages/admin/products.vue
@@ -41,6 +41,23 @@
           label 简介
           input(v-model='edited.intro' @keyup='editedIntro')
         .input-group
+          lable 图片
+          .upload-images
+            .img(v-for='(item, index) in edited.images')
+              img(:src='imageCDN + item + "?imageView2/1/format/jpg/q/75/imageslim"')
+              .tools
+                .material-icon(@click='deleteImg(index)') delete
+          .upload-btn
+            svg(width='53px', height='37px', viewbox='0 0 53 37', version='1.1', xmlns='http://www.w3.org/2000/svg', xmlns:xlink='http://www.w3.org/1999/xlink')
+                g#Page-1(stroke='none', stroke-width='1', fill='none', fill-rule='evenodd')
+                  g#ic_backup_black_24px(transform='translate(-1.000000, -6.000000)')
+                    polygon#Shape(points='0 0 55 0 55 55 0 55')
+                    path#outline(d='M42.6907609,20.750 3727 C41.2853571,13.6200155 35.0230435,8.26708075 27.5,8.26708075 C21.5270342,8.26708075 16.339441,11.6565839 13.7559783,16.6168323 C7.535,17.2781988 2.69875776,22.5484627 2.69875776,28.9347826 C2.69875776,35.7757919 8.25836957,41.3354037 15.0993789,41.3354037 L41.9673913,41.3354037 C47.671677,41.3354037 52.3012422,36.7058385 52.3012422,31.0015528 C52.3012422,25.5452795 48.0643634,21.1223913 42.6907609,20.7503727 Z', stroke='#78909C', stroke-width='3', :stroke-dasharray='upload.dasharray', :stroke-dashoffset='upload.dashoffset')
+                    path#Shape(d='M42.6907609,20.7503727 C41.2853571,13.6200155 35.0230435,8.26708075 27.5,8.26708075 C21.5270342,8.26708075 16.339441,11.6565839 13.7559783,16.6168323 C7.535,17.2781988 2.69875776,22.5484627 2.69875776,28.9347826 C2.69875776,35.7757919 8.25836957,41.3354037 15.0993789,41.3354037 L41.9673913,41.3354037 C47.671677,41.3354037 52.3012422,36.7058385 52.3012422,31.0015528 C52.3012422,25.5452795 48.0643634,21.1223913 42.6907609,20.7503727 Z M31.6335404,26.8680124 L31.6335404,35.1350932 L23.3664596,35.1350932 L23.3664596,26.8680124 L17.1661491,26.8680124 L27.5,16.5341615 L37.8338509,26.8680124 L31.6335404,26.8680124 Z', fill='#CFD8DC', fill-rule='nonzero')
+            br
+            .text 上传图片
+            input(type='file' @change='uploadImg($event)')
+        .input-group
           label 参数
           .parameters
             .inputs(v-for='(item, index) in edited.parameters')
@@ -62,6 +79,8 @@
 
 <script>
 import { mapState } from 'vuex'
+import randomToken from 'random-token'
+import Uploader from 'qiniu-web-uploader'
 import vSnackbar from '../../components/snackbar.vue'
 
 export default {
@@ -143,6 +162,46 @@ export default {
 
     removeParameter(index) {
       this.edited.parameters.splice(index, 1)
+    },
+
+    async getUptoken () {
+      let res = await axios.get('/api/qiniu/token', {
+        params: { key }
+      })
+
+      return res.data.data
+    },
+
+    async uploadImg(e) {
+      this.upload.dashoffset = this.upload.dasharray
+
+      let file = e.target.files[0]
+      let key = randomToken(32)
+
+      key = `products/${key}`
+      let token = await this.getUptoken(key)
+
+      let uptoken = {
+        uptoken: token,
+        key: Buffer.from(key).toString('base64')
+      }
+
+      // Uploader.QINIU_UPLOAD_URL = '//up-z2.qiniu.com' // 我是华南区
+
+      let uploader = new Uploader(file, uptoken)
+
+      uploader.on('progress', () => {
+        // 获取上传进度
+        let dashoffset = this.upload.dasharray * (1 - uploader.percent)
+        // 设置上传进度
+        this.upload.dashoffset = dashoffset
+        // 正式启动上传
+        let res = await uploader.upload()
+
+        uploader.cancel()
+        console.log(res)
+        this.edited.images.push(res.key)
+      })
     }
   },
 
