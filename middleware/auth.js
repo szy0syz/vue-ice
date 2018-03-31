a8e5c2b6b1250062aa2ccc0a1be433c28a8f87f6
szy0syz <szy0syz@gmail.com>
2018-04-01 00:41:26 +0800

    test

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 export default function ({store, redirect}) {
   console.log('进入了中间件auth.js')
-  // if (!store.state.user || !store.state.user.email) {
-  //   return redirect('/admin/login')
-  // }
+  if (!store.state.user || !store.state.user.email) {
+    return redirect('/admin/login')
+  }
 }
--- middleware/wechat-auth.js
+++ middleware/wechat-auth.js
@@ -3,7 +3,8 @@ export default function ({ store, route, redirect }) {
     let {fullPath} = route
 
     fullPath = encodeURIComponent(fullPath.substr(1))
-
+    console.log('wechat-auth 中间件')
+    console.log(fullPath)
     return redirect(`/wechat-redirect?visit=${fullPath}`)
   }
 }
