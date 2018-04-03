c60b3ee1e80d1703031871a4d517f776a44bbb1a
szy0syz <szy0syz@gmail.com>
2018-04-03 23:35:39 +0800

    U 删除多余注释

--- middleware/wechat-auth.js
+++ middleware/wechat-auth.js
@@ -6,7 +6,6 @@ export default function ({ store, route, redirect }) {
     // 删除第一个字符“/”
     fullPath = encodeURIComponent(fullPath.substr(1))
     console.log('wechat-auth 中间件')
-    console.log('fullPath: ', fullPath)
     return redirect(`/wechat-redirect?visit=${fullPath}`)
   }
 }
