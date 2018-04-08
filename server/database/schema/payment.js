e9c4e72cca3b88563ff076d73f7f223d793f3ee2
Jerry Shi <szy0syz@gmail.com>
2018-04-08 13:59:25 +0800

    add 9-4 新增payment订单Schema

---
+++ server/database/schema/payment.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const Mixed = Schema.Types.Mixed
+const ObjectId = Schema.Types.ObjectId
+
+const PaymentSchema = new Schema({
+  user: {
+    type: ObjectId,
+    ref: 'User'
+  },
+  product: {
+    type: ObjectId,
+    ref: 'Product'
+  },
+  payType: String,
+  totalFee: Number,
+  name: String,
+  phoneNumber: String,
+  address: String,
+  description: String,
+  order: Mixed,
+  success: {
+    type: Number,
+    default: 0  // 0:unfinished 1:finished
+  },
+  meta: {
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now()
+    }
+  }
+})
+
+PaymentSchema.pre('save', function (next) {
+  if (this.isNew) {
+    this.meta.createdAt = this.meta.updatedAt = Date.now()
+  } else {
+    this.meta.updatedAt = Date.now()
+  }
+
+  next()
+})
+
+mongoose.model('Payment', PaymentSchema)
