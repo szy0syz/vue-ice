d91241c7b5698a5c7586c5142e9c83187ca8de92
Jerry Shi <szy0syz@gmail.com>
2018-04-08 14:13:16 +0800

    update 创建服务端路由中创建订单功能

--- server/routes/wechat.js
+++ server/routes/wechat.js
@@ -1,34 +1,69 @@
-import { controller, get, post } from '../decorator/router'
-import config from '../config'
-import reply from '../wechat/reply'
-import wechatMiddle from '../wechat-lib/middleware'
-import { signature, redirect, oauth } from '../controllers/wechat'
+import { controller, get, post, required } from "../decorator/router";
+import config from "../config";
+import reply from "../wechat/reply";
+import wechatMiddle from "../wechat-lib/middleware";
+import { signature, redirect, oauth } from "../controllers/wechat";
 
-@controller('')
+@controller("")
 export class WechatController {
-  @get('/wechat-hear')
+  @get("/wechat-hear")
   async wechatHear(ctx, next) {
-    const middle = wechatMiddle(config.wechat, reply)
+    const middle = wechatMiddle(config.wechat, reply);
     await middle(ctx, next)
   }
 
-  @post('/wechat-hear')
+  @post("/wechat-hear")
   async wechatPostHear(ctx, next) {
-    const middle = wechatMiddle(config.wechat, reply)
+    const middle = wechatMiddle(config.wechat, reply);
     await middle(ctx, next)
   }
 
-  @get('/wechat-signature')
+  @post("/wechat-pay")
+  @required({ body: ["productId", "name", "phoneNumber", "address"] })
+  async createOrder(ctx, next) {
+    // 有时候用代理ip返回值修复
+    const ip = ctx.ip.replace("::fff:", "");
+    const session = ctx.session;
+    const { productId, name, phoneNumber, address } = ctx.request.body; // post的body
+
+    // 查询商品是否有效
+    const product = await Product.findOne({
+      _id: productId
+    }).exec()
+
+    if (!product) return (ctx.body = { success: false, err: "这个宝贝不存在" });
+
+    try {
+      // 查询用户是否以前登录过
+      let user = await user.findOne({ unionid: session.user.unionid }).exec();
+      // 如果没登录过
+      if (!user) {
+        user = new user({
+          openid: [session.user.openid],
+          unionid: session.user.unionid,
+          nickname: session.user.nickname,
+          address: session.user.address,
+          province: session.user.province,
+          country: session.user.country,
+          city: session.user.city,
+          sex: session.user.sex,
+          headimgurl: session.user.headimgurl
+        })
+      }
+    } catch (err) {}
+  }
+
+  @get("/wechat-signature")
   async wechatSignature(ctx, next) {
     await signature(ctx, next)
   }
 
-  @get('/wechat-redirect')
+  @get("/wechat-redirect")
   async wechatRedirect(ctx, next) {
     await redirect(ctx, next)
   }
 
-  @get('/wechat-oauth')
+  @get("/wechat-oauth")
   async wechatOAuth(ctx, next) {
     await oauth(ctx, next)
   }
