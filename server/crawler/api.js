9969da154bde3eaac1394f82d59e8551a617e734
szy0syz <szy0syz@gmail.com>
2018-04-13 15:05:03 +0800

    clean

--- server/crawler/api.js
+++ server/crawler/api.js
@@ -1,5 +1,6 @@
 import rp from 'request-promise'
-import R from 'ramda'
+// import R from 'ramda'
+// import { resolve } from 'path'
 import _ from 'lodash'
 import { writeFileSync } from 'fs'
 
@@ -8,7 +9,7 @@ let characters = []
 const sleep = time => new Promise(resolve => setTimeout(resolve, time))
 
 export const getAPICharacters = async (page = 1) => {
-  console.log('page: ',page)
+  console.log('page: ', page)
   const url = `https://www.anapioficeandfire.com/api/characters?page=${page}&pageSize=50`
   console.log('开始爬取第 ' + page + ' 页数据')
   let body = await rp(url)
@@ -17,7 +18,7 @@ export const getAPICharacters = async (page = 1) => {
   console.log('body.length~~  ', body.length)
   if (body.length < 50) {
     console.log('数据爬去完毕')
-    return
+    // return
   } else {
     console.log('爬取到50个数据')
     writeFileSync('./server/crawler/api.json', JSON.stringify(characters, null, 2), 'utf8')
