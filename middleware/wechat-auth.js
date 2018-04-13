e0a71f947eb17e0f1c977bddd5434ede6952a8c7
szy0syz <szy0syz@gmail.com>
2018-04-13 20:40:55 +0800

    fix 到线上调试去

--- middleware/wechat-auth.js
+++ middleware/wechat-auth.js
@@ -2,10 +2,10 @@ export default function ({ store, route, redirect }) {
   console.log('中间件~~~~~~authUser', store.state.authUser)
   if (!store.state.authUser) {
     let { fullPath } = route
-    // console.log(route)
+    console.log(route)
     // 删除第一个字符“/”
     fullPath = encodeURIComponent(fullPath.substr(1))
-    console.log('wechat-auth 中间件')
+    console.log('wechat-auth 中间件fullPath:', fullPath)
     return redirect(`/wechat-redirect?visit=${fullPath}`)
   }
 }
