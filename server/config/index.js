862d00265921e1199ffa97b0d6db3ff52afad7b1
szy0syz <szy0syz@gmail.com>
2018-04-13 18:52:26 +0800

    fix 修复require(resolve())的Critical dependency问题

--- server/config/index.js
+++ server/config/index.js
@@ -1,12 +1,13 @@
 import _ from 'lodash'
-import { resolve } from 'path'
+// import { resolve } from 'path'
 
 const host = process.env.HOST || 'localhost'
 const env = process.env.NODE_ENV || 'development'
-const conf = require(resolve(__dirname, `./${env}.json`))
+
+// const confPath = resolve(__dirname, `./${env}.json`)
+
+// 同步加载json配置文件
+const conf = require(`./${env}.json`)
 
 // 用lodash合并
-export default _.assign({
-  env,
-  host
-}, conf)
+export default _.assign({ env, host }, conf)
