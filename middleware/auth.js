97ec4c02d43ba10a4693a86784f81c723a1f2f69
szy0syz <szy0syz@gmail.com>
2018-04-15 01:40:18 +0800

    clean 注释

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,4 @@
 export default function ({store, redirect}) {
-  console.log('进入了中间件auth.js')
   // 这里只有user.email有值，才表示是网站注册用户。如果是微信客户则没有email  || !store.state.user.email
   if (!store.state.user) {
     return redirect('/admin/login')
