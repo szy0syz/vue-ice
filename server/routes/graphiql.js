d02752d0b6248e5081be87c164e815cdea710f27
szy0syz <szy0syz@gmail.com>
2018-07-25 23:09:51 +0800

    update 12-2 配置 GraphQL 到 Nodejs 项目中

---
+++ server/routes/graphiql.js
@@ -0,0 +1,26 @@
+import { controller, post, get } from '../decorator/router'
+import { graphqlKoa, graphiqlKoa } from 'apollo-server-koa'
+
+@controller('')
+export class GraphqlController {
+  // @post('/graphql')
+  // async postGraphql(ctx, next) {
+  //   await graphqlKoa({
+  //     schema: schema
+  //   })(ctx, next)
+  // }
+
+  // @get('/graphql')
+  // async getGraphql(ctx, next) {
+  //   await graphqlKoa({
+  //     schema: schema
+  //   })(ctx, next)
+  // }
+
+  @get('/graphiql')
+  async getGraphqlKoa(ctx, next) {
+    await graphiqlKoa({
+      endpointURL: '/graphql'
+    })(ctx, next)
+  }
+}
