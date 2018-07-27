18c796456c9931e6fc0fef9ae16ad69cc8c2b00f
szy0syz <szy0syz@gmail.com>
2018-07-27 23:47:00 +0800

    clean 代码清洁

--- server/database/plugins/meta.js
+++ server/database/plugins/meta.js
@@ -5,7 +5,6 @@ module.exports = exports = function lastModifiedPlugin(schema) {
     } else {
       // ☆ 郑重提示：mongoose对于数据会执行 save 中间件length次
       if (this.meta && this.meta.updatedAt) {
-        console.log('!~!~!~~', this.meta.updatedAt)
         this.meta.updatedAt = Date.now()
       }
     }
